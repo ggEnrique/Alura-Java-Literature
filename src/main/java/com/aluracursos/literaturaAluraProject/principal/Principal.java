ced2405b85f19f58099783af6b957f29a70b7e3f
Enrique Garcia <gg.enrique2015@gmail.com>
2024-06-13 08:11:12 -0700

    added principal class and created menu for user

--- src/main/java/com/aluracursos/literaturaAluraProject/principal/Principal.java
+++ src/main/java/com/aluracursos/literaturaAluraProject/principal/Principal.java
@@ -1,13 +1,16 @@
 package com.aluracursos.literaturaAluraProject.principal;
 
+import com.aluracursos.literaturaAluraProject.model.Datos;
+import com.aluracursos.literaturaAluraProject.model.DatosLibros;
 import com.aluracursos.literaturaAluraProject.service.ConsumoAPI;
 import com.aluracursos.literaturaAluraProject.service.ConvierteDatos;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
-    int input = 0;
+    int input = -1;
     Scanner scanner = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos convierteDatos = new ConvierteDatos();
@@ -17,9 +20,10 @@ public class Principal {
 //        System.out.println(json);
 //        var datos = convierteDatos.obtenerDatos(json, Datos.class);
 //        System.out.println(datos);
-        while (true) {
+        while (input != 0) {
             System.out.println("""
-                    Choose the an option with 1 - 7:
+                    **THIS PROGRAM IS IN SPANGLISH**
+                    Choose the an option with 1 - 5 or 0 para salir/exit:
                     1 - buscar libro por título
                     2 - listar libros registrados
                     3 - listar autores registrados
@@ -28,22 +32,63 @@ public class Principal {
                     0 - salir
                     """);
             input = scanner.nextInt();
+            scanner.nextLine();
             switch (input) {
                 case 1:
+                    buscarLibroPorTitulo();
+                    break;
                 case 2:
+                    listarLibrosRegistrados();
+                    break;
                 case 3:
+                    listarAutoresRegistrados();
+                    break;
                 case 4:
+                    listarAutoresPorFecha();
+                    break;
                 case 5:
+                    listarLibrosByIdioma();
+                    break;
                 case 0:
-                    System.out.println("Saliendo...");
+                    System.out.println("Saliendo del programa...");
+                    System.out.println("Closing program...");
                     scanner.close();
                     break;
                 default:
-                    System.out.println("Not a valid choice, try again.");
+                    System.out.println("Not a valid choice, try again...");
             }
 
         }
 
+
+    }
+
+    private void listarLibrosByIdioma() {
+    }
+
+    private void listarAutoresPorFecha() {
+    }
+
+    private void listarAutoresRegistrados() {
+    }
+
+    private void listarLibrosRegistrados() {
+    }
+
+    private void buscarLibroPorTitulo() {
+        System.out.println("Enter the titulo del libro que quieras search for");
+        var tituloLibro = scanner.nextLine();
+        var json = consumoAPI.obtenerDatos(URL_BASE + "?search=" + tituloLibro.replace(" ", "+"));
+        var datos = convierteDatos.obtenerDatos(json, Datos.class);
+        Optional<DatosLibros> librosBuscado = datos.resultados().stream()
+                .filter(l->l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                .findFirst();
+        if(librosBuscado.isPresent()){
+            System.out.println("Libro Econtrado");
+            System.out.println(librosBuscado.get());
+        }else {
+            System.out.println("Book not found");
+        }
 
     }
 }
